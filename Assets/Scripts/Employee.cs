9bbaeab1a0f5f7687343f673f65f9f06336f2412
ugacode <NA>
2023-02-02 16:34:08 +0100

    Initial Employee

---
+++ Assets/Scripts/Employee.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Employee : MonoBehaviour
+{
+    public string Name;
+    public string AvatarPath;
+    public int CodeRate; // K Lines per Q
+    public int ExperienceYears;
+    public int BugFixRate; // per Q
+    public int BugCreationRate; // Per 10K code lines
+    public ConscientiousnessRating Conscientiousness;
+    public OpennessRating Openness;
+}
+
+public enum ConscientiousnessRating {
+    Low = 0,
+    Medium = 1,
+    High = 2
+}
+
+public enum OpennessRating {
+    Low = 0,
+    Medium = 1,
+    High = 2
+}
