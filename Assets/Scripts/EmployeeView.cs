96a68314834ab80afea952af9d37d5387708898f
Aviv Sheriff <avivsheriff@gmail.com>
2023-02-03 21:59:34 -0500

    Avatar Images + Fonts
    Added avatar image importing from JSON, moved avatar folder to resources (for resource load), and changed fonts to Roboto.

--- Assets/Scripts/EmployeeView.cs
+++ Assets/Scripts/EmployeeView.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EmployeeView : MonoBehaviour
 {
@@ -19,19 +20,25 @@ public class EmployeeView : MonoBehaviour
 
         var experienceProperty = gameObject.transform.Find("Property Panel/Experience");
         var experiencePropertyText = experienceProperty.GetComponent<TextMeshProUGUI>();
-        experiencePropertyText.text = $"Years of experience: {employeeData.ExperienceYears}";
+        experiencePropertyText.text = $"<b>Years of experience:</b> {employeeData.ExperienceYears}";
 
         var codingRateProperty = gameObject.transform.Find("Property Panel/CodingSpeed");
         var codingRatePropertyText = codingRateProperty.GetComponent<TextMeshProUGUI>();
-        codingRatePropertyText.text = $"Code production rate: {employeeData.CodeRate} lines per quarter";
+        codingRatePropertyText.text = $"<b>Code production rate:</b> {employeeData.CodeRate} lines per quarter";
 
         var conscientiousnessProperty = gameObject.transform.Find("Property Panel/Conscientiousness");
         var conscientiousnessPropertyText = conscientiousnessProperty.GetComponent<TextMeshProUGUI>();
-        conscientiousnessPropertyText.text = $"Conscientiousness: {employeeData.Conscientiousness}";
+        conscientiousnessPropertyText.text = $"<b>Conscientiousness:</b> {employeeData.Conscientiousness}";
 
         var creativityProperty = gameObject.transform.Find("Property Panel/Creativity");
         var creativityPropertyText = creativityProperty.GetComponent<TextMeshProUGUI>();
-        creativityPropertyText.text = $"Creativity: {employeeData.Openness}";
+        creativityPropertyText.text = $"<b>Creativity:</b> {employeeData.Openness}";
+
+        var avatar = gameObject.transform.Find("Avatar");
+        var avatarImage = Resources.Load<Sprite>($"{employeeData.AvatarPath}");
+        avatar.GetComponent<Image>().sprite = avatarImage;
+        Debug.Log(avatar);
+        Debug.Log(avatarImage);
     }
 
     public void EmployeeHireClicked()
